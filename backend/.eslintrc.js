32dfd2cde034ad6133b70b03444c359eb2dd4894
Xuan-Hien Dieu <79910980+dieuxuanhien@users.noreply.github.com>
2025-12-26 15:47:48 +0700

    chore: update ESLint ignore pattern for prisma directory

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js','seed/'],
+  ignorePatterns: ['.eslintrc.js','backend/prisma/*'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
